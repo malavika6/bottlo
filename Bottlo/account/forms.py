33bd9134d54529bedeab04e1c24434f84b53db18
malavika6 <malavikavinod681@gmail.com>
2023-05-30 11:04:13 +0530

    done

--- Bottlo/account/forms.py
+++ Bottlo/account/forms.py
@@ -2,6 +2,15 @@ from django import forms
 from .models import Account
 
 class Registrationform(forms.ModelForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={ 'placeholder':'First name'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Last name'}))
+    email = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Email'}))
+    phone_number = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Mobile number'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Password'}))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Confirm Password'}))
     class Meta:
         model = Account
-        fields = ['username','email','phone_number','password']
+        fields = ['first_name','last_name','email','phone_number','password']
+
+
+         
--- Bottlo/account/views.py
+++ Bottlo/account/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from .forms import Registrationform
+from. models import Account
 
 def signup(request):
+
     form = Registrationform()
+    if request.method == 'POST':
+        form = Registrationform(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            phone_number = form.cleaned_data['phone_number']
+            password = form.cleaned_data['password1']
+            user = Account.objects.create_user(first_name = first_name, last_name = last_name, phone_number = phone_number, email = email, password = password)
+            user.save()
+            request.session['email']=email
     context = {
         'form':form,
     }
