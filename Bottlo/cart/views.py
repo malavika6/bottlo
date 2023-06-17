abb109bc3a16a7d12afb0df7ac819e76e97b8174
malavika6 <malavikavinod681@gmail.com>
2023-06-17 09:03:38 +0530

    update

--- Bottlo/cart/views.py
+++ Bottlo/cart/views.py
@@ -46,13 +46,16 @@ def cart(request, total=0 ,quantity=0,cart_items =None):
         for cart_item in cart_items:
             total+=(cart_item.product.price*cart_item.quantity)
             quantity+=cart_item.quantity
-        tax = ()
+        tax = (2*total)/100
+        grand_total=total+tax
     except ObjectDoesNotExist:
         pass
     context = {
         'total':total,
         'quantity' : quantity,
-        'cart_items' : cart_items
+        'cart_items' : cart_items,
+        'tax': tax,
+        'grand_total':grand_total,
 
     }
 
