ac4581e48238b3f6b92c89cd9f8f6241f9c9fa4b
malavika6 <malavikavinod681@gmail.com>
2023-06-20 21:44:42 +0530

    done

--- Bottlo/order/views.py
+++ Bottlo/order/views.py
@@ -1,13 +1,65 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from cart.models import Cartitem
+from . forms import OrderForm
+from . models import Order
+from store.models import Product
+import datetime
 
-def place_order(request):
+
+
+
+def place_order(request,total = 0, quantity = 0):
     current_user = request.user
     cart_items = Cartitem.objects.filter(user=current_user)
-    cart_count =cart_items.count()
+    cart_count = cart_items.count()
     if cart_count <= 0:
         return redirect('store')
     
-    
-    
-    
+    grand_total = 0
+    tax = 0
+    for cart_item in cart_items:
+        total += (cart_item.product.price * cart_item.quantity)
+        quantity += cart_item.quantity
+    tax = int(.12 * total)
+    grand_total = total + tax 
+        
+    if request.method == "POST":
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            data= Order()
+            data.user = current_user
+            data.first_name = form.cleaned_data['first_name']
+            data.last_name = form.cleaned_data['last_name']
+            data.address_line_1 = form.cleaned_data['address_line_1']
+            data.address_line_2 = form.cleaned_data['address_line_2']
+            data.city = form.cleaned_data['city']
+            data.state = form.cleaned_data['state']
+            data.country = form.cleaned_data['country']
+            data.pincode = form.cleaned_data['pincode']
+            data.email = form.cleaned_data['email']
+            data.phone_number = form.cleaned_data['phone']
+            data.order_note = form.cleaned_data['order_note']
+            data.order_total = grand_total
+            data.tax = tax
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            # generating order id for each order
+            yr = int(datetime.date.today().strftime('%Y'))
+            dt = int(datetime.date.today().strftime('%d'))
+            mt = int(datetime.date.today().strftime('%m'))
+            d = datetime.date(yr,mt,dt)
+            current_date = d.strftime("%Y%m%d")
+            order_number = current_date + str(data.id)
+            data.order_number=order_number
+            data.save()
+            return redirect('checkout')
+    else:
+        return redirect('checkout')
+            
+            
+            
+            
+            
+            
+            
+        
