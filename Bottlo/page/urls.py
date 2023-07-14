4c5ec3fbcf37b573e50dfae7f8b9b0dbc3eb2482
malavika6 <malavikavinod681@gmail.com>
2023-07-14 12:38:18 +0530

    order cancel

--- Bottlo/page/urls.py
+++ Bottlo/page/urls.py
@@ -6,5 +6,7 @@ from . import views
 urlpatterns = [
     path('',views.home,name='home'),
     path('profile/',views.profile,name='profile'),
+    path('order_detail/<int:order_id>/',views.order_detail,name="order_detail"),
+    path('order_detail/cancel_order/<int:order_id>/', views.cancel_order, name='cancel_order'),
     # path('profile/add_address',views.add_address,name='add_address'),
 ]
--- Bottlo/page/views.py
+++ Bottlo/page/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from store.models import Product
 from category.models import category
-from order.models import Order
+from order.models import Order,OrderProduct
 from account.models import AddressBook
 from account.forms import AddressForm
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
@@ -35,6 +35,49 @@ def profile(request):
         
     }
     return render(request, "account/profile.html", context)
+
+
+def order_detail(request, order_id):
+    try:
+        ordr_products = OrderProduct.objects.filter(order__order_number=order_id)
+        print(ordr_products,"hi")
+        order = Order.objects.get(order_number=order_id)
+        print(order_id,"k")
+        
+        subtotal = 0
+        for i in ordr_products:
+            subtotal += i.product_price*i.quantity
+        tax = (2 * subtotal) / 100
+        grand_total = subtotal + tax    
+        context = {
+            'ordr_products': ordr_products,
+            'tax':tax,
+            'order': order,
+            'subtotal' : subtotal,
+            'grand_total': grand_total
+        }
+        print(ordr_products,"hello")
+    except Order.DoesNotExist:
+        # Handle the case when the order does not exist
+        context = {
+            'error_message': 'Order does not exist.'
+        }
+        print(context)
+    
+    return render(request, 'account/order_detail.html', context)
+
+
+def cancel_order(request, order_id):
+    order = get_object_or_404(Order,order_number=order_id)
+    
+    if order.status == 'Cancelled':
+        messages.warning(request, "This order has already been cancelled.")
+    else:
+        order.status = 'Cancelled'
+        order.save()
+        messages.success(request, "Order successfully cancelled.")
+    
+    return redirect('order_detail', order_id=order_id)
 
 # def  add_address(request):
 #     form = AddressForm()
